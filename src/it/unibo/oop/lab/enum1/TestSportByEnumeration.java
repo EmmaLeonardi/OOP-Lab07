0ab3ce65fe732caddae60b1206d7137146705bb7
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2021-11-08 11:49:13 +0100

    Created tests of SportsByEnumeration

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -12,18 +14,38 @@ package it.unibo.oop.lab.enum1;
  */
 public final class TestSportByEnumeration {
 
-    private TestSportByEnumeration() { }
+    private TestSportByEnumeration() {
+    }
 
     /**
-     * @param args
-     *            ignored
+     * @param args ignored
      */
     public static void main(final String... args) {
-        /*
-         * [TEST DEFINITION]
-         * 
-         * By now, you know how to do it
-         */
-        // TODO
+
+	SportSocialNetworkUserImpl<User> rmaria = new SportSocialNetworkUserImpl<>("Maria", "Rossi", "rmaria", 20);
+	SportSocialNetworkUserImpl<User> vlucia = new SportSocialNetworkUserImpl<>("Lucia", "Verdi", "vlucia", 16);
+	SportSocialNetworkUserImpl<User> bsara = new SportSocialNetworkUserImpl<>("Sara", "Bianchi", "bsara", 30);
+
+	// Maria follows bike, soccer and volley
+	rmaria.addSport(Sport.BIKE);
+	rmaria.addSport(Sport.SOCCER);
+	rmaria.addSport(Sport.VOLLEY);
+
+	// Lucia follows only volley
+	vlucia.addSport(Sport.VOLLEY);
+
+	// Sara follows F1, soccer and MotoGP
+	bsara.addSport(Sport.F1);
+	bsara.addSport(Sport.SOCCER);
+	bsara.addSport(Sport.MOTOGP);
+	bsara.addSport(Sport.VOLLEY);
+
+	//Tests
+	System.out.println("Maria follows bike: " + rmaria.hasSport(Sport.BIKE));
+	System.out.println("Lucia doesn't follow F1: " + !vlucia.hasSport(Sport.F1));
+	System.out.println("Sara follows soccer: " + bsara.hasSport(Sport.SOCCER));
+	System.out.println("Sara, Maria and Lucia all follow volley: "
+		+ (vlucia.hasSport(Sport.VOLLEY) && rmaria.hasSport(Sport.VOLLEY) && bsara.hasSport(Sport.VOLLEY)));
+
     }
 }
