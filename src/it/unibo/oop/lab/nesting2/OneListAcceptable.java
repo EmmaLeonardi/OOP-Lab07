43bb0fbff9cc9900efb598580ecc83f7dd19a4f7
Emma Leonardi <eleonardi.mobile@gmail.com>
2021-11-09 11:55:40 +0100

    One List Acceptable now works as intended

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -8,10 +8,16 @@ public class OneListAcceptable<T> implements Acceptable<T> {
 	private final List<T> listAcceptable;
 	private int index;
 
+	/**
+	 * {@inheritDoc} 
+	 * */
 	public OneListAcceptable(List<T> list) {
 		this.listAcceptable = new ArrayList<T>(list);
 	}
 
+	/**
+	 * {@inheritDoc} 
+	 * */
 	public Acceptor<T> acceptor() {
 		this.index = 0;
 		return new OneListAcceptor();
@@ -22,17 +28,27 @@ public class OneListAcceptable<T> implements Acceptable<T> {
 		private OneListAcceptor() {
 		}
 
+		/**
+		 * {@inheritDoc} 
+		 * */
 		public void accept(T newElement) throws ElementNotAcceptedException {
-			if (OneListAcceptable.this.listAcceptable.get(OneListAcceptable.this.index).equals(newElement)) {
-				OneListAcceptable.this.index++;
+			if (OneListAcceptable.this.listAcceptable.size() > OneListAcceptable.this.index) {
+				if (OneListAcceptable.this.listAcceptable.get(OneListAcceptable.this.index).equals(newElement)) {
+					OneListAcceptable.this.index++;
+				} else {
+					throw new ElementNotAcceptedException(newElement);
+				}
 			} else {
 				throw new ElementNotAcceptedException(newElement);
 			}
 
 		}
 
+		/**
+		 * {@inheritDoc} 
+		 * */
 		public void end() throws EndNotAcceptedException {
-			if (OneListAcceptable.this.index + 1 == OneListAcceptable.this.listAcceptable.size()) {
+			if (OneListAcceptable.this.index == OneListAcceptable.this.listAcceptable.size()) {
 				// The acceptor has arrived at the last element
 			} else {
 				throw new EndNotAcceptedException();
